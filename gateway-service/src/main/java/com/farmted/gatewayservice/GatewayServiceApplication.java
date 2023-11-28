b4e5ed31c223c602b3202e22d7e64977df531a43
Taehk <taehk1013@gmail.com>
2023-11-28 16:01:02 +0900

    [RESTORE] 게이트웨이

--- gateway-service/src/main/java/com/farmted/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/farmted/gatewayservice/GatewayServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// 실행확인
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
