14faa9cfb465e473b0de24afc122d0aa4b24152d
HYESOO KIM <khwater1017@naver.com>
2023-12-04 23:12:05 +0900

    [FEAT #45 ] Schedule 진행하여 상태값 전달 로직

--- product-service/src/main/java/com/farmted/productservice/controller/api/ApiController.java
+++ product-service/src/main/java/com/farmted/productservice/controller/api/ApiController.java
@@ -3,6 +3,7 @@ package com.farmted.productservice.controller.api;
 import com.farmted.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class ApiController {
 
     private final ProductService productService;
 
+    @Scheduled(cron ="*/60 * * * * *")
     @GetMapping("/endAuctions")
     public ResponseEntity<?> endAuctionToProduct(){
        productService.endAuctionFromProduct();
