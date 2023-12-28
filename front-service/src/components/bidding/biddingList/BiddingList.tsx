4fbed9e62446edbd5f24d2b8c423a0bc871e4599
HYESOO KIM <khwater1017@naver.com>
2023-12-28 18:40:22 +0900

    [FEAT #87 ] 추가 입찰 신청 시 컴포넌트 구현

--- front-service/src/components/bidding/biddingList/BiddingList.tsx
+++ front-service/src/components/bidding/biddingList/BiddingList.tsx
@@ -1,5 +1,9 @@
+// BiddingList.tsx
+
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import BiddingModalComponent from './BiddingModalComponent';
+
 
 interface BiddingResponseDto {
   biddingPrice: number;
@@ -9,6 +13,7 @@ interface BiddingResponseDto {
   stock: number;
   source: string;
   image: string;
+  boardUuid: string; // boardUuid를 추가
 }
 
 const AuctionDetailContainer = styled.div`
@@ -18,27 +23,34 @@ const AuctionDetailContainer = styled.div`
 const AuctionDetailTable = styled.table`
   width: 100%;
   margin-bottom: 16px;
+  border-collapse: collapse;
 `;
 
 const AuctionDetailRow = styled.tr`
   display: flex;
   justify-content: space-between;
+  border-bottom: 1px solid #d6d6d6;
+  cursor: pointer;
 `;
 
 const AuctionDetailTitle = styled.th`
   font-size: 18px;
   margin-bottom: 8px;
   text-align: left;
+  padding: 10px;
 `;
 
 const AuctionDetailContent = styled.td`
   font-size: 16px;
   margin-bottom: 8px;
   text-align: right;
+  padding: 10px;
 `;
 
 const BiddingList = () => {
   const [biddingList, setBiddingList] = useState<BiddingResponseDto[]>([]);
+  const [selectedBiddingItem, setSelectedBiddingItem] = useState<BiddingResponseDto | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const memberUuid = '6036e646-97d3-4e53-aee4-f88e00b57a4b';
 
   useEffect(() => {
@@ -72,37 +84,55 @@ const BiddingList = () => {
     };
   }, [memberUuid]);
 
+  const handleRowClick = (biddingItem: BiddingResponseDto) => {
+    setSelectedBiddingItem(biddingItem);
+    setIsModalOpen(true);
+  };
+
   return (
     <AuctionDetailContainer>
       <h2>입찰 목록입니다</h2>
-      <h3>회원 {memberUuid}의 입찰 목록</h3>
+      <h3>사용자님의 입찰 목록</h3>
       {biddingList.length > 0 ? (
-        <AuctionDetailTable>
-          <thead>
-            <AuctionDetailRow>
-              <AuctionDetailTitle>입찰 가격</AuctionDetailTitle>
-              <AuctionDetailTitle>입찰 시간</AuctionDetailTitle>
-              <AuctionDetailTitle>이름</AuctionDetailTitle>
-              <AuctionDetailTitle>재고</AuctionDetailTitle>
-              <AuctionDetailTitle>출처</AuctionDetailTitle>
-              <AuctionDetailTitle>이미지</AuctionDetailTitle>
-            </AuctionDetailRow>
-          </thead>
-          <tbody>
-            {biddingList.map((biddingItem, index) => (
-              <AuctionDetailRow key={index}>
-                <AuctionDetailContent>{biddingItem.biddingPrice}</AuctionDetailContent>
-                <AuctionDetailContent>{biddingItem.biddingTime}</AuctionDetailContent>
-                <AuctionDetailContent>{biddingItem.name}</AuctionDetailContent>
-                <AuctionDetailContent>{biddingItem.stock}</AuctionDetailContent>
-                <AuctionDetailContent>{biddingItem.source}</AuctionDetailContent>
-                <AuctionDetailContent>
-                  <img src={biddingItem.image} alt={`${biddingItem.name}에 대한 이미지`} />
-                </AuctionDetailContent>
+        <>
+          <AuctionDetailTable>
+            <thead>
+              <AuctionDetailRow>
+                <AuctionDetailTitle>입찰 가격</AuctionDetailTitle>
+                <AuctionDetailTitle>입찰 시간</AuctionDetailTitle>
+                <AuctionDetailTitle>이름</AuctionDetailTitle>
+                <AuctionDetailTitle>재고</AuctionDetailTitle>
+                <AuctionDetailTitle>판매처</AuctionDetailTitle>
+                <AuctionDetailTitle>이미지</AuctionDetailTitle>
               </AuctionDetailRow>
-            ))}
-          </tbody>
-        </AuctionDetailTable>
+            </thead>
+            <tbody>
+              {biddingList.map((biddingItem, index) => (
+                <AuctionDetailRow key={index} onClick={() => handleRowClick(biddingItem)}>
+                  <AuctionDetailContent>{biddingItem.biddingPrice}</AuctionDetailContent>
+                  <AuctionDetailContent>{biddingItem.biddingTime}</AuctionDetailContent>
+                  <AuctionDetailContent>{biddingItem.name}</AuctionDetailContent>
+                  <AuctionDetailContent>{biddingItem.stock}</AuctionDetailContent>
+                  <AuctionDetailContent>{biddingItem.source}</AuctionDetailContent>
+                  <AuctionDetailContent>
+                    <img src={biddingItem.image} alt={`${biddingItem.name}에 대한 이미지`} />
+                  </AuctionDetailContent>
+                </AuctionDetailRow>
+              ))}
+            </tbody>
+          </AuctionDetailTable>
+
+          {/* 모달 컴포넌트 */}
+          {isModalOpen && selectedBiddingItem && (
+            <BiddingModalComponent
+              boardUuid={selectedBiddingItem.boardUuid}
+              memberUuid={memberUuid}
+              onClose={() => setIsModalOpen(false)}
+              isOpen={isModalOpen}
+              closeModal={() => setIsModalOpen(false)}
+              modalContent={`${selectedBiddingItem.biddingPrice}`}          />
+          )}
+        </>
       ) : (
         <p>입찰 목록이 없습니다.</p>
       )}
